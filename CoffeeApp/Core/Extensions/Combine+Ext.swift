e35aee341e9e5840d69d90fd47238c78d4e70e9f
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-15 21:42:44 +0330

    add combine extension

---
+++ CoffeeApp/Core/Extensions/Combine+Ext.swift
@@ -0,0 +1,40 @@
+//
+//  Combine+Ext.swift
+//  rocket
+//
+//  Created by mohsen mokhtari on 6/18/23.
+//
+
+import Combine
+
+enum AsyncError: Error {
+    case finishedWithoutValue
+}
+
+extension AnyPublisher {
+    func async() async throws -> Output {
+        try await withCheckedThrowingContinuation { continuation in
+            var cancellable: AnyCancellable?
+            var finishedWithoutValue = true
+            cancellable = first()
+                .sink { result in
+                    switch result {
+                    case .finished:
+                        if finishedWithoutValue {
+                            continuation.resume(throwing: AsyncError.finishedWithoutValue)
+                        }
+                    case let .failure(error):
+                        continuation.resume(throwing: error)
+                    }
+                    cancellable?.cancel()
+                } receiveValue: { value in
+                    finishedWithoutValue = false
+                    continuation.resume(with: .success(value))
+                }
+        }
+    }
+    
+    
+}
+
+
