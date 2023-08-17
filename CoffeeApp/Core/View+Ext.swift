dd1402ff0567250a2e9ae35bfdffde5db832c482
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-17 22:48:29 +0330

    add view extensions

--- CoffeeApp/Core/View+Ext.swift
+++ CoffeeApp/Core/View+Ext.swift
@@ -19,4 +19,36 @@ extension View {
     func getRect()->CGRect{
         return UIScreen.main.bounds
     }
+    
+    func border(_ width: CGFloat, _ color: Color) -> some View {
+        self
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background {
+            RoundedRectangle(cornerRadius: 5, style: .continuous)
+                .stroke(color, lineWidth: width)
+        }
+    }
+    
+    func closeKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    func disabledWithOpacity(_ condition: Bool) -> some View {
+        self
+            .disabled(condition)
+            .opacity(condition ? 0.6 : 1)
+    }
+    
+    func fillView(_ color: Color) -> some View {
+        self
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background {
+            RoundedRectangle(cornerRadius: 5, style: .continuous)
+                .fill(color)
+        }
+    }
 }
+
+
