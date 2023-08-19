c2dc3662579f58713857126cb54e99dbf11d143d
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-19 15:27:22 +0330

    add default keys

---
+++ CoffeeApp/Presentation/Shared/DefaultsKey.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultsKey.swift
+//  CoffeeApp
+//
+//  Created by mohsen mokhtari on 8/15/23.
+//
+
+import Defaults
+
+extension Defaults.Keys {
+    static let user = Key<User?>("user")
+    static let userId = Key<String>("userId",default: "-1")
+}
